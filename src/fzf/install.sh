2604a0b7f6d95bfffc9ff318d2e576b271cb4d7f
Daniel Braun <7925710+braun-daniel@users.noreply.github.com>
2024-10-10 20:53:19 +0000

    Fixes;

--- src/fzf/install.sh
+++ src/fzf/install.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
-# This script installs fzf and all necessary tools for a DevContainer feature.
 
-# Exit script on any error
 set -e
 
 # Install git
@@ -10,5 +8,5 @@ apt-get update
 apt-get install -y git
 
 # Install fzf
-su - "$_REMOTE_USER" -c "git clone --depth 1 https://github.com/junegunn/fzf.git ${_REMOTE_USER_HOME}/.fzf"
+su - "$_REMOTE_USER" -c "git clone --depth=1 https://github.com/junegunn/fzf.git ${_REMOTE_USER_HOME}/.fzf"
 su - "$_REMOTE_USER" -c "${_REMOTE_USER_HOME}/.fzf/install"
