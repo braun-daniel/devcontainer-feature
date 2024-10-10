3f0e452d0c7fac31b7e1ef8c6dee7093137ce299
Daniel Braun <7925710+braun-daniel@users.noreply.github.com>
2024-10-10 21:46:15 +0000

    Fixes;

--- src/fzf/install.sh
+++ src/fzf/install.sh
@@ -7,13 +7,6 @@ echo "Installing git and wget..."
 apt-get update
 apt-get install -y git wget
 
-# # Install Go
-# echo "Installing Go..."
-# GO_VERSION="1.20.5"
-# wget https://golang.org/dl/go${GO_VERSION}.linux-amd64.tar.gz
-# tar -C /usr/local -xzf go${GO_VERSION}.linux-amd64.tar.gz
-# export PATH=$PATH:/usr/local/go/bin
-
 # Install fzf as the remote user
 _REMOTE_USER=${_REMOTE_USER:-"root"}
 _REMOTE_USER_HOME=$(eval echo "~${_REMOTE_USER}")
