0502040eb1bb45fe1b851d138e580372c94e8c6a
Daniel Braun <7925710+braun-daniel@users.noreply.github.com>
2024-10-10 21:03:24 +0000

    Fixes;

--- src/fzf/install.sh
+++ src/fzf/install.sh
@@ -2,10 +2,10 @@
 
 set -e
 
-# Install git
+# Install dependencies
 echo "Installing git..."
 apt-get update
-apt-get install -y git
+apt-get install -y git wget
 
 # Install fzf
 su - "$_REMOTE_USER" -c "git clone --depth=1 https://github.com/junegunn/fzf.git ${_REMOTE_USER_HOME}/.fzf"
--- test/spaceship/test.sh
+++ test/spaceship/test.sh
@@ -6,7 +6,7 @@ set -e
 source dev-container-features-test-lib
 
 # Definition specific tests
-check "check for spaceship" su - "$_REMOTE_USER" -c "spaceship --version"
+check "check for spaceship" spaceship --version
 
 # Report result
 reportResults
